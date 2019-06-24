ed9083fe27fbd661e881b08111651859f2b8b588
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-06-25 02:00:52 +0300

    fix

--- writer.py
+++ writer.py
@@ -18,24 +18,26 @@ class ExifWriter:
         if self.template[keyword].lower().startswith("python'"):
             code = re.search(r"python'{(.*?)\}", self.template[keyword]).groups()[0]
             if_statement = code.split('else')[0].strip()
-            # if_value = if_statement.split('if')[0].strip()
+            if_value = if_statement.split('if')[0].strip()
             if_rule = if_statement.split('if')[1].strip()
             else_statement  = code.split('else')[1].strip()
             for data in self.raw_data:
                 if data['prop'].get('slug') == if_rule:
-                    if data['prop'].get('datatype') == 'enum':
-                        _tags = self.delimiter.join(tag.get('value') for tag in data.get('value') if tag.get('value'))
-                        return _tags
-                    elif data['prop'].get('datatype') == 'bool':
-                        return str(data.get('value'))
-                    elif data['prop'].get('datatype') == 'date':
-                        _dt = datetime.strptime(data.get('value'), "%Y-%m-%dT%H:%M:%SZ")\
-                            .strftime('%Y:%m:%d %H:%M:%S[%f][%z]')
-                        return str(_dt)
-                    if re.search(r'-.{2}', keyword.lower()):
-                        _lang = re.search(r'-.{2}', keyword.lower()).group().replace('-','')
-                        if data['lang'] == _lang:
-                            return data.get('value')
+                    for _data in self.raw_data:
+                        if _data['prop'].get('slug') == if_value:
+                            if re.search(r'-.{2}', keyword.lower()):
+                                _lang = re.search(r'-.{2}', keyword.lower()).group().replace('-','')
+                                if _data['lang'] == _lang:
+                                    return _data.get('value')
+                            elif _data['prop'].get('datatype') == 'enum':
+                                _tags = self.delimiter.join(tag.get('value') for tag in _data.get('value') if tag.get('value'))
+                                return _tags
+                            elif _data['prop'].get('datatype') == 'bool':
+                                return str(_data.get('value'))
+                            elif _data['prop'].get('datatype') == 'date':
+                                _dt = datetime.strptime(_data.get('value'), "%Y-%m-%dT%H:%M:%SZ")\
+                                    .strftime('%Y:%m:%d %H:%M:%S[%f][%z]')
+                                return str(_dt)
             return else_statement
 
 
@@ -68,7 +70,6 @@ class ExifWriter:
         _writable_data = []
 
         for keyword in self.template:
-            print(keyword, self.template[keyword])
             _val = self._get_value(keyword)
             _writable_data.append(f"-{keyword}={_val}")
         return _writable_data
